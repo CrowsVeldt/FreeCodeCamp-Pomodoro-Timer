d349f90676dec6629d6753b54e13af7e5646ed19
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-08-20 18:56:07 +0300

    Added basic structure to .js, minor fixes.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 (function () {
   'use strict;'
+  
+  //Model - Start time, End Time, Work Length, Break Length
+  
+  const time = {}
+  
+  //View - The timer, itself
+  
+  const view = {}
+  
+  //Controller - Play/Pause Button, Time Input
+  
+  const input = {}
+  
 }())
