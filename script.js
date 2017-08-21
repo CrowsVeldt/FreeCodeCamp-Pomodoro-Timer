660e5b5eecf267a62451611579fcdeff9deaed32
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-08-21 08:58:28 +0300

    changed input elements from type "text" to type "number". Semantic markup FTW

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 (function () {
   'use strict;'
-  
-  //Model - Holds variables for the current work length, break length, start time, and end time
-  
+
+  // Model - Holds variables for the current work length, break length, start time, and end time
+
   const time = {}
-  
-  //View - Updates the view as the model is updated
-  
+
+  // View - Updates the view as the model is updated
+
   const view = {}
-  
-  //Controller - Accepts user input, updates the model as needed
-  
+
+  // Controller - Accepts user input, updates the model as needed
+
   const input = {}
-  
 }())
