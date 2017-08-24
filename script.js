433440083d679c870fc6a5ea0925ea58f0635865
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-08-24 09:30:02 +0300

    added eventListener to the button that updates the timer

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 (function () {
   'use strict;'
   
-  var testObj = {
-    active: true,
+  const currentTimer = {
+    active: false,
     startTime: 0,
-    currentTime: 0,
-    pomodoro: 25,
+    pomodoro: 50,
     sBreak: 5,
-    lBreak: 15
+    lBreak: 15,
   }
   
-  // When currentTime is equal to startTime.getMinutes() + pomodoro % 60 the time is up.
+  const button = document.getElementById('play-reset')
   
-  // When the user presses the 'play' button, use setTimeout to create a timeout according to the value of 'Pomodoro length'. Play a sound at the end.
+  button.addEventListener('click', function(){
+    
+  // When currentTime is equal to startTime.getMinutes() + pomodoro % 60 the time is up.
+    
+    currentTimer.active = true
+    currentTimer.startTime = new Date().getMinutes()
+    currentTimer.endTime = (currentTimer.startTime + currentTimer.pomodoro) % 60
+    
+  })
 }())
