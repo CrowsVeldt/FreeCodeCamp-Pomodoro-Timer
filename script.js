7bb76a143b4e21264e3269df948c05817f17d559
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-08-24 09:51:02 +0300

    First working iteration of the timer!

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 (function () {
   'use strict;'
-  
+
   const currentTimer = {
     active: false,
     startTime: 0,
-    pomodoro: 50,
+    pomodoro: 1,
     sBreak: 5,
-    lBreak: 15,
+    lBreak: 15
   }
-  
+
   const button = document.getElementById('play-reset')
-  
-  button.addEventListener('click', function(){
-    
+
+  button.addEventListener('click', function () {
   // When currentTime is equal to startTime.getMinutes() + pomodoro % 60 the time is up.
-    
+
     currentTimer.active = true
     currentTimer.startTime = new Date().getMinutes()
     currentTimer.endTime = (currentTimer.startTime + currentTimer.pomodoro) % 60
-    
+
+    setInterval(function () {
+      console.log('checking')
+
+      let check = new Date().getMinutes()
+
+      if (check === currentTimer.endTime) {
+        currentTimer.startTime = check
+        currentTimer.endTime = (check + currentTimer.pomodoro) % 60
+
+        console.log('restart')
+      }
+    }, 1000)
   })
 }())
