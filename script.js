082cce3383536006bd8b29d6ba02a400a38aa985
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-08-25 09:58:48 +0300

    moved timer functions into the timer object

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 (function () {
   'use strict;'
+  
+  const startButton = document.getElementById('start')
+  const stopButton = document.getElementById('stop')
 
   const currentTimer = {
     active: false,
@@ -7,49 +10,40 @@
     pomodoro: 1,
     sBreak: 5,
     lBreak: 15,
-    timerID: 0
-  }
+    timerID: 0,
+    
+    checkTime: function checkTime () {
+      console.log('check')
 
-  const startButton = document.getElementById('start')
-  const resetButton = document.getElementById('reset')
+      let check = new Date().getTime()
 
-  const input = {
+      if (check >= currentTimer.endTime) {
+        currentTimer.endTime = check + (currentTimer.pomodoro * 60000)
+
+        console.log('restart')
+      }
+    },
     
-    function
+    startTimer: function startTimer () {
+      if (currentTimer.active === false) {
+        currentTimer.active = true
+        currentTimer.startTime = new Date().getTime()
+        currentTimer.endTime = currentTimer.startTime + (currentTimer.pomodoro * 60000)
+
+        currentTimer.timerID = setInterval(currentTimer.checkTime, 1000)
+      }
+    },
     
-    
+    stopTimer: function stopTimer () {
+      console.log('stopped')
+      if (currentTimer.active === true) {
+        currentTimer.active = false
+
+        clearInterval(currentTimer.timerID)
+      }
+    }  
   }
   
-  function checkTime () {
-    console.log('check')
-
-    let check = new Date().getTime()
-
-    if (check >= currentTimer.endTime) {
-      currentTimer.endTime = check + (currentTimer.pomodoro * 60000)
-
-      console.log('restart')
-    }
-  }
-
-  function startTimer () {
-    if (currentTimer.active === false) {
-      currentTimer.active = true
-      currentTimer.startTime = new Date().getTime()
-      currentTimer.endTime = currentTimer.startTime + (currentTimer.pomodoro * 60000)
-
-      currentTimer.timerID = setInterval(checkTime, 1000)
-    }
-  }
-
-  function resetTimer () {
-    if (currentTimer.active === true) {
-      currentTimer.active = false
-
-      clearInterval(currentTimer.timerID)
-    }
-  }
-
-  startButton.addEventListener('click', startTimer)
-  resetButton.addEventListener('click', resetTimer)
+  startButton.addEventListener('click', currentTimer.startTimer)
+  stopButton.addEventListener('click', currentTimer.stopTimer)
 }())
