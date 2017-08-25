436fa021a950603bf571d4500212e438b549640a
CrowsVeldt <Snorri.Jonas@gmail.com>
2017-08-25 09:42:43 +0300

    Finished timer code refactor. As it turns out I was calling startTimer AND resetTimer at the same time (facepalm)

--- script.js
+++ script.js
@@ -10,48 +10,39 @@
     timerID: 0
   }
 
-  const button = document.getElementById('play-reset')
+  const startButton = document.getElementById('start')
+  const resetButton = document.getElementById('reset')
 
-  // Extract the code from addEventListener to these functions, call them from the eventListener
-  
-    function checkTime () {
-    
+  function checkTime () {
     console.log('check')
 
     let check = new Date().getTime()
 
     if (check >= currentTimer.endTime) {
-
       currentTimer.endTime = check + (currentTimer.pomodoro * 60000)
 
       console.log('restart')
     }
-    
   }
 
   function startTimer () {
-    //debugger
     if (currentTimer.active === false) {
       currentTimer.active = true
       currentTimer.startTime = new Date().getTime()
       currentTimer.endTime = currentTimer.startTime + (currentTimer.pomodoro * 60000)
 
       currentTimer.timerID = setInterval(checkTime, 1000)
-      checkTime()
     }
   }
 
   function resetTimer () {
-    // debugger
     if (currentTimer.active === true) {
       currentTimer.active = false
 
       clearInterval(currentTimer.timerID)
     }
   }
-  
 
-
-  button.addEventListener('click', startTimer)
-  button.addEventListener('click', resetTimer)
+  startButton.addEventListener('click', startTimer)
+  resetButton.addEventListener('click', resetTimer)
 }())
