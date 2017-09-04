d0f96489b81604a5bc2744964b13aa7325684d64
CrowsVeldt <zechy.mahler@gmail.com>
2017-09-04 10:59:56 +0300

    installed webpack-dev-server and updated webpack.config, added run script to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ var path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.bundle.js'
