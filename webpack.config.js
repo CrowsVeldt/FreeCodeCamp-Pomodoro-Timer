734ed4ac8bfeaf735d6f5ef491671d1780e90d9c
CrowsVeldt <zechy.mahler@gmail.com>
2017-09-04 10:14:46 +0300

    added build script to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 
 module.exports = {
-  entry: './src/index.ds',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.bundle.js'
