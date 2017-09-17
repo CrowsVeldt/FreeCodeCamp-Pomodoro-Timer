77f58ffab2837e923c97fb4ac9c195b435ed1c2a
CrowsVeldt <zechy.mahler@gmail.com>
2017-09-18 00:43:36 +0300

    removed input placeholder variables as they are no longer needed (woohoo!)

--- src/input/input.js
+++ src/input/input.js
@@ -18,11 +18,3 @@ export function createSettingsView () {
   createInputElement('short Break', '5')
   createInputElement('long Break', '15')
 }
-
-/* Placeholder variables for input from input elements */
-export const userInputs = {
-  pomodoroLength: 0.1,
-  shortBreakLength: 0.2,
-  longBreakLength: 0.3
-}
-/* end of placeholder variables */
