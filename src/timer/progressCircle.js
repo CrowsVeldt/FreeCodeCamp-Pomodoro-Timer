fa543846fc1e709e7b2e19a25768b8beb41141b4
CrowsVeldt <zechy.mahler@gmail.com>
2017-09-23 18:50:49 +0300

    fixed the ratio between the viewbox and the progress circle  radius. Added arguments to createProgressCircle to let it update with the timer

--- src/timer/progressCircle.js
+++ src/timer/progressCircle.js
@@ -1,15 +1,14 @@
 import styles from './progressCircle.css'
 
-export function createProgressCircle (radius, height, width) {
+export function createProgressCircle (radius, viewBox, height, width, amountDone) {
   let circumference = 2 * radius * Math.PI
-  let percent = 100 / 50
 
   let container = document.createElement('div')
   container.setAttribute('id', 'container')
   container.setAttribute('class', styles.container)
 
   let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-  svg.setAttribute('viewBox', '0, 0, ' + radius + ', ' + radius)
+  svg.setAttribute('viewBox', '0, 0, ' + viewBox + ', ' + viewBox)
   svg.setAttribute('preserveAspectRatio', 'xMidYMid meet')
   svg.setAttribute('id', 'svg')
   svg.setAttribute('class', styles.svg)
@@ -19,21 +18,22 @@ export function createProgressCircle (radius, height, width) {
   circle.setAttribute('id', 'circle')
   circle.setAttribute('fill', 'transparent')
   circle.setAttribute('stroke', 'red')
-  circle.setAttribute('stroke-width', '10')
-  circle.setAttribute('r', (radius / 2) - 5)
+  circle.setAttribute('stroke-width', '5%')
+  circle.setAttribute('r', radius)
   circle.setAttribute('cx', '50%')
   circle.setAttribute('cy', '50%')
+  circle.setAttribute('stroke-dasharray', circumference)
 
   let coverCircle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
   coverCircle.setAttribute('class', styles.coverCircle)
   coverCircle.setAttribute('id', 'cover-circle')
   coverCircle.setAttribute('fill', 'transparent')
   coverCircle.setAttribute('stroke', 'lightgrey')
-  coverCircle.setAttribute('stroke-width', '10.2')
-  coverCircle.setAttribute('r', (radius / 2) - 5.1)
+  coverCircle.setAttribute('stroke-width', '5%')
+  coverCircle.setAttribute('r', radius)
   coverCircle.setAttribute('cx', '50%')
   coverCircle.setAttribute('cy', '50%')
-  coverCircle.setAttribute('stroke-dashoffset', -(circumference / 60) * percent)
+  coverCircle.setAttribute('stroke-dashoffset', -(circumference / 360) * amountDone)
   coverCircle.setAttribute('stroke-dasharray', circumference)
 
   svg.appendChild(circle)
