2bd06ae8a96d3229b0d4d8fe94664e2f7463a16c
CrowsVeldt <zechy.mahler@gmail.com>
2017-10-12 09:32:44 +0300

    Introduce functionality to choose an alarm sound from local files
    The initial functionality is all in place, except for adding the file
    to the dropdown menu. I'm going to have to refactor the code for
    picking an alarm first.

--- src/input/alarmPicker.js
+++ src/input/alarmPicker.js
@@ -8,22 +8,24 @@ export function createAlarmPicker () {
   dropdown.setAttribute('id', 'alarmDropdown')
 
   const watchOption = document.createElement('option')
-  watchOption.setAttribute('value', 'Watch Alarm')
+  watchOption.setAttribute('value', 'watchAlarm')
   watchOption.innerHTML = 'Watch Alarm'
 
   const gongOption = document.createElement('option')
-  gongOption.setAttribute('value', 'Gong')
+  gongOption.setAttribute('value', 'gongAlarm')
   gongOption.innerHTML = 'Gong'
 
   dropdown.appendChild(watchOption)
   dropdown.appendChild(gongOption)
 
-  dropdown.addEventListener('change', function (event) {
-    if (document.getElementById('alarmDropdown').value === 'Watch Alarm') {
+  dropdown.addEventListener('change', function (event, optionalUrl) {
+    if (document.getElementById('alarmDropdown').value === 'watchAlarm') {
       chooseAlarmSound(watchAlarm)
-    } else if (document.getElementById('alarmDropdown').value === 'Gong') {
+    } else if (document.getElementById('alarmDropdown').value === 'gong') {
       chooseAlarmSound(gongAlarm)
-    }
+    } /* else if (document.getElementById('alarmDropdown').value === 'userInputAlarm') {
+      chooseAlarmSound(optionalUrl)
+    } */
 
     if (storageAvailable('localStorage') && window.localStorage.getItem('pomodoro')) {
       populateStorage()
@@ -36,4 +38,18 @@ export function createAlarmPicker () {
 export function chooseAlarmSound (name) {
   const alarm = document.getElementById('alarm')
   alarm.setAttribute('src', name)
+}
+
+export function addAlarm (URL, fileName) {
+  chooseAlarmSound(URL)
+
+  // How to get the objectURL to the event listener on the dropdown menu?
+  const alarmDropdown = document.getElementById('alarmDropdown')
+  const userAlarm = document.createElement('option')
+
+  userAlarm.setAttribute('value', 'userInputAlarm')
+  // use String.replace to clean up the fileName here
+  userAlarm.innerHTML = fileName
+
+  alarmDropdown.appendChild(userAlarm)
 }
--- src/input/filePicker.js
+++ src/input/filePicker.js
@@ -1,3 +1,5 @@
+import {addAlarm} from './alarmPicker'
+
 export function createFilePicker () {
   const fileLabel = document.createElement('label')
   fileLabel.setAttribute('for', 'file')
@@ -6,12 +8,20 @@ export function createFilePicker () {
   const filePicker = document.createElement('input')
   filePicker.setAttribute('type', 'file')
   filePicker.setAttribute('id', 'file')
+  filePicker.setAttribute('accept', 'audio/*')
 
   fileLabel.appendChild(filePicker)
 
-  fileLabel.addEventListener('change', function () {
-    // console.log(document.getElementById('file').files[0])
+  filePicker.addEventListener('change', function () {
+    // Check the file is a valid audio file here
+    const selectedFile = document.getElementById('file').files[0]
+    handleFiles(selectedFile, selectedFile.name)
   })
 
   return fileLabel
+}
+
+function handleFiles (file, fileName) {
+  const objectURL = window.URL.createObjectURL(file)
+  addAlarm(objectURL, fileName)
 }
